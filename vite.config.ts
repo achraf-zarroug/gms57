ef32bdd007964588198598197958034e15999947
achraf1934 <achraf.zarroug96@gmail.com>
2025-10-04 03:03:11 +0100

    géneration sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap'; // Importation du plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({
+     hostname: 'https://gms57.fr', 
+    })
+  ],
 })
